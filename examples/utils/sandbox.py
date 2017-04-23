7ec8715fd9a7c6cb043310a9bb43883c9d74910a
FIRAT <yfpayalan@gmail.com>
2017-04-24 01:49:40 +0300

    img read is added

--- examples/utils/sandbox.py
+++ examples/utils/sandbox.py
@@ -5,3 +5,11 @@ c=[[1,2],[1,2]]
 print(tf.shape(a))
 print(tf.shape(b))
 print(tf.shape(c))
+
+
+
+# tensor = decode_png("C:\\Users\\FIRAT\\Desktop\\1.png");
+#bu resim okuyor...
+image = tf.image.decode_png(tf.read_file('C:\\Users\\FIRAT\\Desktop\\1.png'),1)
+sess = tf.Session()
+print(sess.run(image))
